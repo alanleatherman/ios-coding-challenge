f3789ce37aec6022b63d4d06d39fedfee7ca1492
alanleatherman <alan@brandingbrand.com>
2015-11-02 14:01:05 -0500

    ALCodingChallenge-4: altered helper method to be isValidValue to encompass more cases

---
+++ ALCodingChallenge/Supporting Files/ALCodingChallengeHelpers.h
@@ -0,0 +1,13 @@
+//
+//  ALCodingChallengeHelpers.h
+//  ALCodingChallenge
+//
+//  Created by Alan Leatherman on 11/2/15.
+//  Copyright © 2015 Alan Leatherman. All rights reserved.
+//
+
+@interface ALCodingChallengeHelpers : NSObject
+
++ (BOOL)isValidValue:(id)value;
+
+@end
